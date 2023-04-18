bbd35e288864d0c7068bf1c363752edcb2edce74
Marcel Petrick <mail@marcelpetrick.it>
2023-04-18 18:45:52 +0200

    githubYearlyOutputScraper: improved the code without GPT, added comments

--- githubYearlyOutputScraper/githubYearlyOutputScraper.py
+++ githubYearlyOutputScraper/githubYearlyOutputScraper.py
@@ -1,16 +1,21 @@
 # Plan is to make a script, which renders my own github-profile and then screenshots the part of the commit-graph.
 # Doing this for every of the available years and saving this as PNG. Plan is to convert them later to a GIF.
-# Start 20230417_2245
+# Start 20230417_2245 - End 20230417_2330
+# Start 20230418_1840 - End
 
+# How to get those via pip?
 # pip install pillow
 # pip install selenium
+
+# Regenerted the prompt to use Microsoft Edge, because the geckodriver did not work with the Firefox coming from those
+# Windows-Apps - no proper executable. With the limited admin rights on this machine, I choose another path ..
 import os
 import time
 from selenium import webdriver
 from PIL import Image
 
 # Set the path to the Edge WebDriver
-edge_driver_path = os.path.join(os.getcwd(), "msedgedriver.exe")
+edge_driver_path = os.path.join(os.getcwd(), "msedgedriver.exe") # put this to the main partition, no need to adjust
 
 # Set up the browser
 options = webdriver.EdgeOptions()
@@ -22,13 +27,13 @@ browser = webdriver.Edge(executable_path=edge_driver_path, options=options)
 browser.set_window_size(1920, 1080)
 
 # Navigate to the GitHub profile
-url = "https://github.com/marcelpetrick/"
+url = "https://github.com/marcelpetrick/" # set my own repo
 browser.get(url)
 
 # Wait for the page to load
-time.sleep(5)
+time.sleep(3) # TODO really necessary? Maybe reduce this
 
-# Find the commit graph element
+# Find the commit graph element - had to be adjusted due to deprecation. Result from the prompt was not working, pre-2022!
 from selenium.webdriver.common.by import By
 commit_graph = browser.find_element(By.CSS_SELECTOR, "div.js-yearly-contributions") # Code needed to be replaced, because deprecated
 
@@ -38,9 +43,10 @@ commit_graph.screenshot("commit_graph.png")
 # Close the browser
 browser.quit()
 
-# Open the image and crop if necessary
-image = Image.open("commit_graph.png")
-image.show()
-
-# Save the final image
-image.save("commit_graph_cropped.png")
+# TODO this does not crop, lol. Commented the whole block.
+# # Open the image and crop if necessary
+# image = Image.open("commit_graph.png")
+# image.show()
+#
+# # Save the final image
+# image.save("commit_graph_cropped.png")
