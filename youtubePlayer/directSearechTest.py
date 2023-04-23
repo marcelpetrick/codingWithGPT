f11364fc87d8d8978bc213be48bf8e8c7de0f7df
Marcel Petrick <mail@marcelpetrick.it>
2023-04-24 00:10:20 +0200

    youtubePlayer: did 1 min-search for an alternative for the search-query for youtube .. works

---
+++ youtubePlayer/directSearechTest.py
@@ -0,0 +1,13 @@
+# needed a replacement for the suggested, but not working options: why not do a stupid request and parese the resutling page?
+# Taken fron https://codefather.tech/blog/youtube-search-python/ and expanded.
+def directSearch(input):
+    import urllib.request
+    import re
+
+    search_keyword = input.replace(" ", "+")
+    html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
+    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    print("https://www.youtube.com/watch?v=" + video_ids[0])
+
+directSearch("bach jauchzet frohlocket")
+# result: https://www.youtube.com/watch?v=DlwcZT1XVss - which works
