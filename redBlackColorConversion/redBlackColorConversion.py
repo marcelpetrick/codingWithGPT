0f16020542a9dfedc3349c49f3e1c6617acd558b
Marcel Petrick <mail@marcelpetrick.it>
2023-05-02 22:27:07 +0200

    redBlackColorConversion: new approach

--- redBlackColorConversion/redBlackColorConversion.py
+++ redBlackColorConversion/redBlackColorConversion.py
@@ -2,8 +2,10 @@
 import numpy as np
 from PIL import Image
 from sklearn.cluster import KMeans
+import numpy as np
+from PIL import Image
 
-def load_image_and_prepare_array(image_path):
+def convert_to_red_black_white(image_path, output_path):
     try:
         # Open the input image
         image = Image.open(image_path)
@@ -11,52 +13,33 @@ def load_image_and_prepare_array(image_path):
         # Convert the image to RGB mode
         image = image.convert('RGB')
 
-        # Convert the image to a numpy array and reshape it to a 2D array
-        image_data = np.array(image)
-        image_data = image_data.reshape(-1, 3)
-
-        return image, image_data
-
-    except IOError:
-        print(f"Error: Cannot open the image file at {image_path}")
-        return None, None
-    except ValueError as e:
-        print(f"Error: {e}")
-        return None, None
-    except Exception as e:
-        print(f"Error: {e}")
-        return None, None
-
-def process_and_save_image(image_data, output_path):
-    try:
         # Define the target palette (red, black, and white)
-        target_palette = np.array([[255, 0, 0], [0, 0, 0], [255, 255, 255]])
+        target_palette = Image.new('P', (1, 1))
+        target_palette.putpalette([255, 0, 0,  # Red
+                                   0, 0, 0,    # Black
+                                   255, 255, 255] + [0, 0, 0] * 253)  # White and padding
 
-        # Apply k-means clustering
-        kmeans = KMeans(n_clusters=3, init=target_palette, random_state=42, n_init=1)
-        kmeans.fit(image_data)
-        labels = kmeans.predict(image_data)
-        centroids = kmeans.cluster_centers_
+        # Quantize the image using the custom palette
+        quantized_image = image.quantize(palette=target_palette, dither=Image.NONE)
 
-        # Replace each pixel color with the corresponding cluster's centroid color
-        new_image_data = centroids[labels].astype(np.uint8).reshape(image_data.shape)
-
-        # Create a new image with the converted colors
-        new_image = Image.fromarray(new_image_data, mode='RGB')
+        # Convert the quantized image back to RGB mode
+        new_image = quantized_image.convert('RGB')
 
         # Save the converted image as a PNG
         new_image.save(output_path, 'PNG')
         print(f"Image converted successfully and saved to {output_path}")
 
+    except IOError:
+        print(f"Error: Cannot open the image file at {image_path}")
     except ValueError as e:
         print(f"Error: {e}")
     except Exception as e:
         print(f"Error: {e}")
 
 # Example usage:
-input_image_path = 'farbkreis.jpg'
+input_image_path = 'werni.jpg'
 output_image_path = 'werni_out.png'
-image, image_data = load_image_and_prepare_array(input_image_path)
-print("prepped image :)")
-if image_data is not None:
-    process_and_save_image(image_data, output_image_path)
+convert_to_red_black_white(input_image_path, output_image_path)
+input_image_path = 'farbkreis.jpg'
+output_image_path = 'farbkreis_out.png'
+convert_to_red_black_white(input_image_path, output_image_path)
