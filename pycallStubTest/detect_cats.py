366b8e9ac4db8cbce64e86a76c2cd90f339058b1
Marcel Petrick <mail@marcelpetrick.it>
2023-05-10 17:02:37 +0200

    stub: test code for the python-call and processing of value

---
+++ pycallStubTest/detect_cats.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+def detect_cats(path_to_check, frames_to_process):
+    # Since we don't have a specific condition or mechanism to detect cats
+    # I'm using a mock condition here. You need to replace this with your own logic.
+    if os.path.exists(path_to_check) and isinstance(frames_to_process, int):
+        return True
+    return False
+
+if __name__ == "__main__":
+    path_to_check = sys.argv[1]
+    frames_to_process = int(sys.argv[2])
+    result = detect_cats(path_to_check, frames_to_process)
+    sys.exit(0 if result else 1)
