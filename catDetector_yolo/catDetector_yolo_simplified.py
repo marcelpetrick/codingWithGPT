53cac304805c649d1a38e43c3a4f8c82bc14e2fd
Marcel Petrick <mail@marcelpetrick.it>
2023-05-11 14:18:24 +0200

    catDetector_yolo_simplified.py: improved code; removed crap and unneeded output

--- catDetector_yolo/catDetector_yolo_simplified.py
+++ catDetector_yolo/catDetector_yolo_simplified.py
@@ -1,9 +1,9 @@
+# still needed? I guess not
 # pip install Pillow
 
 import os
 import glob
 import sys
-import cv2
 from yolov5 import YOLOv5
 
 def detect_cat(images, yolo):
@@ -15,25 +15,19 @@ def detect_cat(images, yolo):
         double_slash_path = normPath.replace(os.sep, "//")
         print("  -> double_slash_path: ", double_slash_path)
 
-        # Display the image for 2 seconds
-        #cv2.imshow('Image', double_slash_path)
-        #cv2.waitKey(500)  # Display for 2000 milliseconds (2 seconds)
-
         results = yolo.predict(image)
-        print("results.names:", results.names)
+        #print("results.names:", results.names)
 
         for result in results.xyxy:
-            print("result:", result)
-            for tensor in result: # attention: had to modify this manually: GPT4 was not able to generate proper code to iterate over the list of tensors, always forgot one dimension .. therefore checking the class name was not working
-                print("tensor:", tensor)
+            #print("result:", result)
+            for tensor in result:
+                #print("tensor:", tensor)
                 class_index = int(tensor[-1].item())  # Get the class index
                 class_name = results.names[class_index]  # Get the class name
                 print(f"Detected object class: {class_name}")
                 if class_name == 'cat':
-                    #cv2.destroyAllWindows()  # Close the image window
                     return True
 
-    #cv2.destroyAllWindows()  # Close the image window
     return False
 
 def str_to_int(s):
@@ -48,17 +42,16 @@ def str_to_int(s):
         # If the conversion is not possible, return None and False
         return None, False
 
-
 def get_image_paths(path, maxNumber):
     image_paths = []
 
     current_directory = os.getcwd()
-    print(f"current working directory: {current_directory}")
+    #print(f"current working directory: {current_directory}")
 
     # The glob.glob function returns a list of paths matching the specified pathname pattern.
     # The pattern here is "*.[jpg|gif]", which matches all .jpg and .gif files in the directory specified by path.
     all_images = sorted(glob.glob(os.path.join(path, "*.jpg")) + glob.glob(os.path.join(path, "*.gif")))
-    print("all images:", all_images)
+    #print("all images:", all_images)
 
     # Loop through the images and add paths to the image_paths list until the specified number is reached
     for image in all_images:
@@ -67,7 +60,7 @@ def get_image_paths(path, maxNumber):
         baseName = os.path.basename(image).split('.')[0]
         image_number, success = str_to_int(baseName)
         if success:
-            print(f"The converted value is {image_number}.")
+            #print(f"The converted value is {image_number}.")
             # Add the image path to the image_paths list if the image number is less than the specified number
             if image_number < maxNumber:
                 image_paths.append(image)
@@ -98,15 +91,19 @@ def detect_cats(path_to_check, frames_to_process):
 
     return False
 
+import time
 # taken from the caller-stub
 if __name__ == "__main__":
+    startTime = time.time()
     path_to_check = sys.argv[1]
     frames_to_process = int(sys.argv[2])
     result = detect_cats(path_to_check, frames_to_process)
+    endTime = time.time()
+    print("cat detection took: ", endTime - startTime, " seconds")
     sys.exit(0 if result else 1)
 
 # test call
-# python catadetector_yolo_simplified.py testdata//mojo 20
+# python catDetector_yolo_simplified.py testdata//mojo 20
 #
 # Detected object class: cat
 # A cat was detected in one of the images.
