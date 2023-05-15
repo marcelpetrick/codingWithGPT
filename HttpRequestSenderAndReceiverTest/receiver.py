4c9088095bd323294ff9558f6e8cc23b3cf0ce8e
Marcel Petrick <mail@marcelpetrick.it>
2023-05-15 19:11:48 +0200

    httpRequestSenderAndReceiverTest: went with the Flask-idea; but has a problem on the server-side

--- HttpRequestSenderAndReceiverTest/receiver.py
+++ HttpRequestSenderAndReceiverTest/receiver.py
@@ -1,9 +1,7 @@
-import socket
+from flask import Flask, request
 import os
-
 from PIL import Image
 import numpy as np
-
 
 def process_image(image_path):
     # Load image
@@ -20,28 +18,23 @@ def process_image(image_path):
     # If the average pixel value is greater than 127 (midpoint of 0-255), return True, else False
     return average_pixel_value > 127
 
+app = Flask(__name__)
 
-def process_data(data):
-    # Save the data to a file
-    with open('/path/to/save/image.jpg', 'wb') as f:
-        f.write(data)
-    # TODO: process the image and get the result
-    # This might take up to 30 seconds
-    result = process_image('mojo.gif')
-    return result
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part', 400
 
-def start_server():
-    host = '0.0.0.0'
-    port = 5000
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file', 400
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((host, port))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            data = conn.recv(1024)
-            result = process_data(data)
-            conn.sendall(bytes(str(result), 'utf-8'))
+    if file:
+        file.save(os.path.join('temp/', file.filename))
+        # TODO: process the image and get the result
+        # This might take up to 30 seconds
+        result = process_image(file.filename)
+        return {'result': result}
 
-start_server()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- HttpRequestSenderAndReceiverTest/sender.py
+++ HttpRequestSenderAndReceiverTest/sender.py
@@ -1,12 +1,13 @@
 import requests
 
 def send_file(filename):
-    url = 'http://server-ip:5000/upload'
+    # url = 'http://server-ip:5000/upload'
+    url = 'http://127.0.0.1:5000/upload'
     files = {'file': open(filename, 'rb')}
 
     response = requests.post(url, files=files)
     if response.ok:
-        print(response.json())  # should print the result: {'result': True/False}
+        print("response: ", response.json())  # should print the result: {'result': True/False}
     else:
         print('Error:', response.text)
 
