f4951ee4e5198a7dbc26bbc672cba6079ca48654
Marcel Petrick <mail@marcelpetrick.it>
2023-05-18 15:03:40 +0200

    senderReceiver: fixed necessary dir (should be created in the future instead)

--- HttpRequestSenderAndReceiverTest/sender.py
+++ HttpRequestSenderAndReceiverTest/sender.py
@@ -1,14 +1,15 @@
 import requests
 
 def send_file(filename):
-    url = 'http://marcel-precision3551:5000/upload'
     #url = 'http://127.0.0.1:5000/upload'
+    #url = 'http://marcel-precision3551:5000/upload'
+    url = 'http://malina400:5000/upload'
     files = {'file': open(filename, 'rb')}
 
     response = requests.post(url, files=files)
     if response.ok:
-        print("response: ", response.json())  # should print the result: {'result': True/False}
+        print('response:', response.json())  # should print the result: {'result': True/False}
     else:
         print('Error:', response.text)
 
-send_file('tempgifs/cam0.gif')
+send_file('mojo.gif')
