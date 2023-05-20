3dcd24065198f8b4a944426e88ab73e2a73e9317
Marcel Petrick <mail@marcelpetrick.it>
2023-05-20 15:33:57 +0200

    sender.py: added a bit more testing and benchmarking

--- HttpRequestSenderAndReceiverTest/sender.py
+++ HttpRequestSenderAndReceiverTest/sender.py
@@ -1,9 +1,10 @@
 import requests
+import time
 
 def send_file(filename):
-    #url = 'http://127.0.0.1:5000/upload'
-    url = 'http://marcel-precision3551:5000/upload'
-    #url = 'http://malina400:5000/upload'
+    #url = 'http://127.0.0.1:5000/upload' # local development
+    #url = 'http://marcel-precision3551:5000/upload' # host: laptop
+    url = 'http://malina400:5000/upload' # host: the rpi
     files = {'file': open(filename, 'rb')}
 
     response = requests.post(url, files=files)
@@ -12,6 +13,15 @@ def send_file(filename):
     else:
         print('Error:', response.text)
 
-send_file('mojo.gif') # FALSE
-send_file('cat.gif') # FALSE
-send_file('frame09.gif') # TRUE
+def processWithTiming(filename):
+    startTime = time.time()
+    send_file(filename)
+    print(f"  processing took {time.time()-startTime}")
+
+processWithTiming('mojo.gif') # FALSE
+processWithTiming('cat.gif') # FALSE
+processWithTiming('frame09.gif') # TRUE
+
+# stays within 1.2 seconds with request and processing
+for i in range(0,9):
+    processWithTiming('mojo.gif')
