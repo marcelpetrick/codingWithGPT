04892f0380f8b4f77975e1beb58e6868e8b60a24
Marcel Petrick <mail@marcelpetrick.it>
2023-06-14 17:17:55 +0200

    gitLogPreparator.py: fix the prefix as well - to save time for the user :)

--- gitLogCleaner/gitLogPreparator.py
+++ gitLogCleaner/gitLogPreparator.py
@@ -8,6 +8,18 @@ def extract_ticket_number(commit_msg):
     # If found, return it, otherwise return an empty string
     return match.group(1) if match else ''
 
+def process_line(line):
+    # Find first occurrence of ':' character
+    index = line.find(':')
+
+    # If ':' exists, remove everything before it
+    # Keep the rest of the line as it is
+    if index != -1:
+        return line[index + 1:].strip()
+    else:
+        # If there's no ':' character in the line, return the line as it is
+        return line.strip()
+
 def get_commit_info(commit_range):
     # Run 'git log --oneline' command
     result = subprocess.run(['git', 'log', '--oneline', commit_range],
@@ -20,9 +32,9 @@ def get_commit_info(commit_range):
                                     text=True, capture_output=True, check=True).stdout
         ticket_number = extract_ticket_number(commit_msg)
         if ticket_number:
-            print(f'{line} ({ticket_number})')
+            print(f'{process_line(line)} ({ticket_number})')
         else:
-            print(line)
+            print(process_line(line))
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -30,3 +42,13 @@ if __name__ == '__main__':
         sys.exit(1)
 
     get_commit_info(sys.argv[1])
+
+# call from the repo which should be examined like
+#
+# $ python3 ../repo/gitLogCleaner/gitLogPreparator.py v1.12.0..v1.12.1
+# v1.12.1: adapted for the release (12345)
+# warning-fix: two different types in the conditional expressions.
+# API update v3.14.15 (23456)
+# Major new feature (26789)
+# Minor feature (18992)
+# [..]
