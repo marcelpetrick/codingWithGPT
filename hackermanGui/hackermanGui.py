b62467dd98b29aa1c2dbfbd20051ac853b39adbe
Marcel Petrick <mail@marcelpetrick.it>
2023-06-16 23:55:16 +0200

    hackermanGui: starting the UI for the chat-prompter

---
+++ hackermanGui/hackermanGui.py
@@ -0,0 +1,97 @@
+import sys
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QLineEdit, QPushButton, QTextEdit, QLabel, QScrollBar
+
+
+class ProcessThread(QThread):
+    resultReady = pyqtSignal(str)
+
+    def __init__(self, prompt):
+        super().__init__()
+        self.prompt = prompt
+
+    def run(self):
+        # Dummy implementation of processPrompt()
+        # Replace this with your actual processing logic
+        result = self.processPrompt(self.prompt)
+        self.resultReady.emit(result)
+
+    def processPrompt(self, prompt):
+        # Dummy implementation
+        # Replace this with your actual processing logic
+        return "Processed: " + prompt
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Prompt Processor")
+        self.resize(600, 400)
+
+        # Create the main layout
+        main_layout = QVBoxLayout()
+
+        # Create the input area
+        input_layout = QHBoxLayout()
+        self.prompt_line_edit = QLineEdit()
+        self.prompt_line_edit.setMaxLength(2048)
+        self.go_button = QPushButton("Go")
+        self.go_button.clicked.connect(self.processButtonClicked)
+        input_layout.addWidget(self.prompt_line_edit)
+        input_layout.addWidget(self.go_button)
+
+        # Create the scrolling view
+        self.result_text_edit = QTextEdit()
+        self.result_text_edit.setReadOnly(True)
+
+        # Create the footer labels for statistics
+        self.stats_label1 = QLabel("Stat 1:")
+        self.stats_label2 = QLabel("Stat 2:")
+        self.stats_label3 = QLabel("Stat 3:")
+
+        # Add the input area, scrolling view, and footer labels to the main layout
+        main_layout.addLayout(input_layout)
+        main_layout.addWidget(self.result_text_edit)
+        main_layout.addWidget(self.stats_label1)
+        main_layout.addWidget(self.stats_label2)
+        main_layout.addWidget(self.stats_label3)
+
+        # Create a central widget and set the main layout
+        central_widget = QWidget()
+        central_widget.setLayout(main_layout)
+        self.setCentralWidget(central_widget)
+
+    def processButtonClicked(self):
+        prompt = self.prompt_line_edit.text()
+        if prompt:
+            self.disableInput()
+            self.startProcessingThread(prompt)
+
+    def startProcessingThread(self, prompt):
+        self.thread = ProcessThread(prompt)
+        self.thread.resultReady.connect(self.updateResult)
+        self.thread.finished.connect(self.enableInput)
+        self.thread.start()
+
+    def disableInput(self):
+        self.prompt_line_edit.setEnabled(False)
+        self.go_button.setEnabled(False)
+
+    def enableInput(self):
+        self.prompt_line_edit.setEnabled(True)
+        self.go_button.setEnabled(True)
+
+    def updateResult(self, result):
+        self.result_text_edit.append(result)
+
+    def updateStatistics(self, stat1, stat2, stat3):
+        self.stats_label1.setText("Stat 1: {}".format(stat1))
+        self.stats_label2.setText("Stat 2: {}".format(stat2))
+        self.stats_label3.setText("Stat 3: {}".format(stat3))
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
