84725761e7cc4d70b1d6cc1994fc96b5ab8c7a5a
Marcel Petrick <mail@marcelpetrick.it>
2023-08-04 17:28:14 +0200

    basicAccess for Azure: added PAT via ignored textfile; reduced the example

--- azureDevOpsTools/basicAccessAndScrap.py
+++ azureDevOpsTools/basicAccessAndScrap.py
@@ -1,30 +1,33 @@
-import os
-# Fetch the PAT from environment variables
-PAT = os.getenv('PAT')
-print(f'PAT: {PAT}')
-organization_url = 'https://dev.azure.com/bora-devops/P118_HMI/_apis/wit/wiql?api-version=6.0'
-
-from azure.devops.connection import Connection
-from msrest.authentication import BasicAuthentication
-import pprint
-
-# replace these with your own values
-#organization_url = "https://dev.azure.com/your-organization"
-organization_url = 'https://dev.azure.com/bora-devops/P118_HMI'
-pat = PAT #"your-personal-access-token"
-
-credentials = BasicAuthentication('', pat)
-connection = Connection(base_url=organization_url, creds=credentials)
-
-client = connection.clients.get_work_item_tracking_client()
-
-# Sample query: Get all work items
-wiql = "SELECT [System.Id] FROM workitems"
-
-results = client.query_by_wiql(wiql).work_items
-
-# print all work item IDs
-for work_item in results:
-    print(work_item.id)
+import requests
 
 
+def get_pat_from_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.read().strip()
+
+
+def get_projects(pat):
+    url = 'https://dev.azure.com/bora-devops/_apis/projects'
+    headers = {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + pat
+    }
+
+    response = requests.get(url, headers=headers)
+
+    print("status_code:", response.status_code) # 203 is response from cache
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    pat = get_pat_from_file('test_pat_full_access.txt')
+    projects = get_projects(pat)
+    print(f"projects: {projects}")
+    if projects:
+        for project in projects['value']:
+            print('Project: ', project['name'])
+    else:
+        print('Error fetching projects')
