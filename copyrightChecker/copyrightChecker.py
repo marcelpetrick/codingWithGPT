b3fbe8677cad40b514013702b10e067305ed8164
Marcel Petrick <mail@marcelpetrick.it>
2023-08-04 11:59:33 +0200

    copyrightChecker.py: documented the code; added exception handling

--- copyrightChecker/copyrightChecker.py
+++ copyrightChecker/copyrightChecker.py
@@ -4,6 +4,13 @@ import itertools
 
 
 def find_files(directory, pattern):
+    """
+    Yield all the files in the directory that match the pattern.
+
+    :param directory: str, the directory to search.
+    :param pattern: str, the pattern to match files against.
+    :yield: str, the full path of a matching file.
+    """
     for root, dirs, files in os.walk(directory):
         for basename in files:
             if fnmatch.fnmatch(basename, pattern):
@@ -12,6 +19,12 @@ def find_files(directory, pattern):
 
 
 def check_copyright(directory):
+    """
+    Check files in the directory for copyright text and print the files that don't contain it.
+
+    :param directory: str, the directory to check.
+    :return: list, a list of files that don't contain the copyright text.
+    """
     patterns = ['*.h', '*.cpp']
     total_files = 0
     copyrightless_files = 0
@@ -21,33 +34,29 @@ def check_copyright(directory):
     for pattern in patterns:
         for filename in find_files(directory, pattern):
             total_files += 1
-            with open(filename, 'r') as f:
-                lines = list(itertools.islice(f, 100))
-                foundCopyright = False
-                for line in lines:
-                    if copyright_text in line:
-                        foundCopyright = True
-                if not foundCopyright:
-                    #print(filename)
-                    result_list.append(filename)
-                    copyrightless_files += 1
+            try:
+                with open(filename, 'r') as f:
+                    lines = list(itertools.islice(f, 100))
+                    if not any(copyright_text in line for line in lines):
+                        result_list.append(filename)
+                        copyrightless_files += 1
+            except Exception as e:
+                print(f"Error reading file {filename}: {str(e)}")
 
     print(f'Checked {total_files} files. {copyrightless_files} files did not have the copyright line.')
     return result_list
 
 
-def filter_list(input_list : list, filter_string_list : list):
-    # remove all entries from result_list which have a certain string
-    filtered_list = []
-    for entry in input_list:
-        found = False
-        for filter_string in filter_string_list:
-            if filter_string in entry:
-                found = True
-                break
-        if not found:
-            filtered_list.append(entry)
-    return filtered_list
+def filter_list(input_list: list, filter_string_list: list):
+    """
+    Remove all entries from input_list which contain any string from filter_string_list.
+
+    :param input_list: list, the list of strings to filter.
+    :param filter_string_list: list, the list of strings to filter out.
+    :return: list, the filtered list.
+    """
+    return [entry for entry in input_list if not any(filter_string in entry for filter_string in filter_string_list)]
+
 
 #-----------
 directory_to_check = '/home/mpetrick/repos/P118_HMI/'
