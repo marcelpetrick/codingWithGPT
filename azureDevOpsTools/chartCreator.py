f830d2774581f11de001e5404c8dee3ca389278c
Marcel Petrick <mail@marcelpetrick.it>
2023-08-08 18:02:11 +0200

    basic access for Azure DevOps: works and creates a plot, but it is an ugly one.

--- azureDevOpsTools/chartCreator.py
+++ azureDevOpsTools/chartCreator.py
@@ -1,7 +1,68 @@
 import re
 from datetime import datetime
+import ast
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.patches as patches
+
+def extract_tickets_from_file(file_path):
+    """Extracts ticket information from a given text file.
+
+    :param file_path: Path to the text file containing ticket data.
+    :return: A list of dictionaries containing ticket information.
+    """
+    tickets = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            ticket = ast.literal_eval(line.strip())
+            tickets.append(ticket)
+    return tickets
+
+def plot_tickets(tickets, start_date, end_date):
+    """Plots tickets on a timeline using specified start and end dates.
+
+    :param tickets: A list of dictionaries containing ticket information.
+    :param start_date: The start date of the global timescale.
+    :param end_date: The end date of the global timescale.
+    """
+    fig, ax = plt.subplots(figsize=(12, len(tickets) * 0.2))
+    colors = ['red', 'green', 'yellow', 'orange', 'purple']
+
+    # Set the x-axis as a global timescale
+    ax.set_xlim(start_date, end_date)
+    ax.xaxis.set_major_locator(mdates.MonthLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.xticks(rotation=45)
+    plt.title(f'Tickets from {start_date.strftime("%Y-%m-%d")} to {end_date.strftime("%Y-%m-%d")}')
+    plt.yticks([])
+
+    # Plot each ticket
+    for idx, ticket in enumerate(tickets):
+        y_value = len(tickets) - idx
+        created_date = datetime.strptime(ticket['Created Date'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        resolved_date = datetime.strptime(ticket['Resolved Date'], '%Y-%m-%dT%H:%M:%S.%fZ') if ticket['Resolved Date'] else end_date
+        closed_date = datetime.strptime(ticket['Closed Date'], '%Y-%m-%dT%H:%M:%S.%fZ') if ticket['Closed Date'] else end_date
+
+        # Draw the line for the ticket
+        rect = patches.Rectangle((mdates.date2num(created_date), y_value - 0.05), mdates.date2num(resolved_date or closed_date) - mdates.date2num(created_date), 0.1,
+                                 facecolor=colors[idx % len(colors)], edgecolor='black')
+        ax.add_patch(rect)
+
+        # Add the ticket details
+        title_ellipsed = (ticket['Title'][:30] + '...') if len(ticket['Title']) > 30 else ticket['Title']
+        ax.text(mdates.date2num(start_date) - 50, y_value, f"ID: {ticket['ID']} | Type: {ticket['Work Item Type']} | Title: {title_ellipsed}",
+                verticalalignment='center', horizontalalignment='right', fontsize=8)
+
+    plt.gca().invert_yaxis()
+    plt.tight_layout()
+    plt.savefig('tickets_plot.png')
 
 def extract_dates(line):
+    """Extracts dates from a given line.
+
+    :param line: A string containing dates in a specific format.
+    :return: A list of datetime objects representing the extracted dates.
+    """
     dates = []
     matches = re.findall(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d+Z', line)
     for match in matches:
@@ -9,6 +70,11 @@ def extract_dates(line):
     return dates
 
 def find_earliest_and_latest_dates(file_path):
+    """Finds the earliest and latest dates from a given text file.
+
+    :param file_path: Path to the text file containing dates.
+    :return: Strings representing the earliest and latest dates in 'YYYY-MM-DD hh:mm' format.
+    """
     earliest_date = None
     latest_date = None
     with open(file_path, 'r') as file:
@@ -23,10 +89,22 @@ def find_earliest_and_latest_dates(file_path):
     return earliest_date.strftime('%Y-%m-%d %H:%M') if earliest_date else None, latest_date.strftime('%Y-%m-%d %H:%M') if latest_date else None
 
 def main():
+    """Main function to extract ticket information, find global timescale, and plot tickets."""
     file_path = 'collectedUpdate3Tickets_20230808_1736.txt'
-    earliest_date, latest_date = find_earliest_and_latest_dates(file_path)
-    print(f'Earliest date: {earliest_date}')
-    print(f'Latest date: {latest_date}')
+    tickets = extract_tickets_from_file(file_path)
+
+    # Find the global earliest and latest dates
+    all_dates = [datetime.strptime(ticket['Created Date'], '%Y-%m-%dT%H:%M:%S.%fZ') for ticket in tickets]
+    all_dates += [datetime.strptime(ticket['Resolved Date'], '%Y-%m-%dT%H:%M:%S.%fZ') for ticket in tickets if ticket['Resolved Date']]
+    all_dates += [datetime.strptime(ticket['Closed Date'], '%Y-%m-%dT%H:%M:%S.%fZ') for ticket in tickets if ticket['Closed Date']]
+    earliest_date = min(all_dates)
+    latest_date = max(all_dates)
+
+    print(f'Earliest date: {earliest_date.strftime("%Y-%m-%d %H:%M")}')
+    print(f'Latest date: {latest_date.strftime("%Y-%m-%d %H:%M")}')
+
+    plot_tickets(tickets, earliest_date, latest_date)
+
 
 if __name__ == "__main__":
     main()
