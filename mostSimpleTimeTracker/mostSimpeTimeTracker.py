3c1b1dba2f5d1ba8a6bfa9239b3248249bf0d8e4
Marcel Petrick <mail@marcelpetrick.it>
2023-08-16 12:41:05 +0200

    mostSimpletimeTracker: added dividers; making sure the order of the items is fitting; added documentation

--- mostSimpleTimeTracker/mostSimpeTimeTracker.py
+++ mostSimpleTimeTracker/mostSimpeTimeTracker.py
@@ -8,9 +8,11 @@
 # take all that data and put it into the last section (vertical layout for the gui) and show it like this: "item 1: 2 h 1 m; item 2: 1h, .. and so on".
 #
 # is everything clear? impress me. it is really, important to me.
+#
+# ... added way more requests ..
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLineEdit, QRadioButton, QButtonGroup, \
-    QLabel
+    QLabel, QFrame
 from PyQt5.QtCore import QTimer
 import sys
 import datetime
@@ -19,13 +21,27 @@ import configparser
 
 
 def seconds_to_hms(seconds):
+    """
+    Convert seconds to hours, minutes, and seconds format.
+
+    :param int seconds: The number of seconds.
+    :return: A string representation of the time in "h m s" format.
+    :rtype: str
+    """
     h, s = divmod(seconds, 3600)
     m, s = divmod(s, 60)
     return f"{h}h {m}m {s}s"
 
 
 class TimeTrackingApp(QMainWindow):
+    """
+    The main window for the time tracking application.
+    """
+
     def __init__(self):
+        """
+        Initialize the main window.
+        """
         super().__init__()
 
         self.setWindowTitle("Time Tracking Tool")
@@ -33,18 +49,24 @@ class TimeTrackingApp(QMainWindow):
         central_widget = QWidget(self)
         self.setCentralWidget(central_widget)
 
-        self.layout = QVBoxLayout()
-        central_widget.setLayout(self.layout)
+        self.main_layout = QVBoxLayout()
+        central_widget.setLayout(self.main_layout)
 
         # Line edit for entering item names
         self.line_edit = QLineEdit(self)
         self.line_edit.setMaxLength(30)
         self.line_edit.returnPressed.connect(self.add_item)
-        self.layout.addWidget(self.line_edit)
+        self.main_layout.addWidget(self.line_edit)
+
+        # Divider line
+        divider1 = QFrame(self)
+        divider1.setFrameShape(QFrame.HLine)
+        self.main_layout.addWidget(divider1)
 
         # Radio box to display the list of items
+        self.radio_layout = QVBoxLayout()
+        self.main_layout.addLayout(self.radio_layout)
         self.radio_group = QButtonGroup(self)
-        self.layout.addWidget(QLabel("Items:"))
 
         # Timer to log the selected item every second and update the used times
         self.timer = QTimer(self)
@@ -52,22 +74,33 @@ class TimeTrackingApp(QMainWindow):
         self.timer.timeout.connect(self.evaluate)
         self.timer.start(1000)
 
+        # Divider line
+        divider2 = QFrame(self)
+        divider2.setFrameShape(QFrame.HLine)
+        self.main_layout.addWidget(divider2)
+
         # Label to display the evaluation results
         self.results_label = QLabel(self)
-        self.layout.addWidget(self.results_label)
+        self.main_layout.addWidget(self.results_label)
 
         # Load configuration
         self.load_configuration()
 
     def add_item(self):
+        """
+        Add a new item to the radio button group.
+        """
         item_name = self.line_edit.text().strip()
         if item_name:
             radio_button = QRadioButton(item_name, self)
-            self.layout.addWidget(radio_button)
+            self.radio_layout.addWidget(radio_button)
             self.radio_group.addButton(radio_button)
             radio_button.setChecked(True)
 
     def log_selected_item(self):
+        """
+        Log the currently selected item every second.
+        """
         checked_button = None
         for button in self.radio_group.buttons():
             if button.isChecked():
@@ -81,6 +114,10 @@ class TimeTrackingApp(QMainWindow):
                 file.write(f"{item_name}\n")
 
     def evaluate(self):
+        """
+        Evaluate the text file for the current day, count the appearances of each item,
+        and display the results in a label.
+        """
         date = datetime.datetime.now().strftime("%Y-%m-%d")
         try:
             with open(f"{date}.txt", "r") as file:
@@ -93,17 +130,23 @@ class TimeTrackingApp(QMainWindow):
             pass
 
     def load_configuration(self):
+        """
+        Load the configuration from the config.ini file.
+        """
         config = configparser.ConfigParser()
         config.read("config.ini")
         if "items" in config:
             for item_name, status in config["items"].items():
                 radio_button = QRadioButton(item_name, self)
-                self.layout.addWidget(radio_button)
+                self.radio_layout.addWidget(radio_button)
                 self.radio_group.addButton(radio_button)
                 if status == "selected":
                     radio_button.setChecked(True)
 
     def save_configuration(self):
+        """
+        Save the configuration to the config.ini file.
+        """
         config = configparser.ConfigParser()
         config["items"] = {}
         for button in self.radio_group.buttons():
@@ -116,6 +159,11 @@ class TimeTrackingApp(QMainWindow):
             config.write(file)
 
     def closeEvent(self, event):
+        """
+        Handle the close event of the main window.
+
+        :param event: The close event.
+        """
         self.save_configuration()
         super().closeEvent(event)
 
