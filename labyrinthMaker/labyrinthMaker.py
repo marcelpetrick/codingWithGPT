b0612adc1f1c84f52564e28484ccd96184326ac2
Marcel Petrick <mail@marcelpetrick.it>
2023-08-22 14:59:12 +0200

    labyrinthMaker: working, nice mazes; with output

--- labyrinthMaker/labyrinthMaker.py
+++ labyrinthMaker/labyrinthMaker.py
@@ -3,46 +3,59 @@ from PIL import Image, ImageDraw, ImageFont
 import random
 
 
-def generate_labyrinth(width, height, wall_thickness):
-    def add_horizontal_wall(x1, x2, y):
-        for i in range(x1, x2 + 1):
-            for j in range(y - wall_thickness + 1, y + wall_thickness):
-                labyrinth[i][j] = 1
+def generate_iterative_random_maze(width, height, wall_thickness):
+    DIRECTIONS = [
+        (0, -2),  # Up
+        (2, 0),  # Right
+        (0, 2),  # Down
+        (-2, 0)  # Left
+    ]
+    maze = [[1 for _ in range(2 * height + 1)] for _ in range(2 * width + 1)]
+    for x in range(0, 2 * width + 1, wall_thickness):
+        for y in range(0, 2 * height + 1, wall_thickness):
+            maze[x][y] = 0
+    start_x = random.choice(range(1, 2 * width, 2))
+    start_y = random.choice(range(1, 2 * height, 2))
+    maze[start_x][start_y] = 0
 
-    def add_vertical_wall(y1, y2, x):
-        for i in range(y1, y2 + 1):
-            for j in range(x - wall_thickness + 1, x + wall_thickness):
-                labyrinth[j][i] = 1
+    # Using a stack for iterative implementation
+    stack = [(start_x, start_y)]
 
-    def recursive_division(x1, x2, y1, y2):
-        if x2 - x1 < 2 * wall_thickness + 1 or y2 - y1 < 2 * wall_thickness + 1:
-            return
-        hx = random.randint(x1 + wall_thickness, x2 - wall_thickness) // wall_thickness * wall_thickness
-        hy = random.randint(y1 + wall_thickness, y2 - wall_thickness) // wall_thickness * wall_thickness
-        add_horizontal_wall(x1, x2, hy)
-        add_vertical_wall(y1, y2, hx)
+    while stack:
+        x, y = stack[-1]
+        random.shuffle(DIRECTIONS)
 
-        hole_x = random.choice([x for x in range(x1 + wall_thickness, x2 - wall_thickness + 1) if x != hx])
-        hole_y = random.choice([y for y in range(y1 + wall_thickness, y2 - wall_thickness + 1) if y != hy])
-        for i in range(hole_x - wall_thickness + 1, hole_x + wall_thickness):
-            labyrinth[i][hy] = 0
-        for i in range(hole_y - wall_thickness + 1, hole_y + wall_thickness):
-            labyrinth[hx][i] = 0
+        # Check for available neighbors
+        available_directions = []
+        for dx, dy in DIRECTIONS:
+            next_x, next_y = x + dx, y + dy
+            if 0 < next_x < 2 * width and 0 < next_y < 2 * height and maze[next_x][next_y] == 1:
+                available_directions.append((dx, dy))
 
-        recursive_division(x1, hx - wall_thickness, y1, hy - wall_thickness)
-        recursive_division(hx + wall_thickness, x2, y1, hy - wall_thickness)
-        recursive_division(x1, hx - wall_thickness, hy + wall_thickness, y2)
-        recursive_division(hx + wall_thickness, x2, hy + wall_thickness, y2)
+        if available_directions:
+            dx, dy = random.choice(available_directions)
+            next_x, next_y = x + dx, y + dy
+            maze[next_x][next_y] = 0
+            maze[x + dx // 2][y + dy // 2] = 0
+            stack.append((next_x, next_y))
+        else:
+            # No available neighbors, backtrack
+            stack.pop()
 
-    labyrinth = [[0] * (height * wall_thickness) for _ in range(width * wall_thickness)]
-    recursive_division(0, width * wall_thickness - 1, 0, height * wall_thickness - 1)
-    labyrinth[1][0] = 0
-    labyrinth[1][1] = 0
+    for _ in range((width * height) // 5):
+        x = random.choice(range(1, 2 * width, 2))
+        y = random.choice(range(1, 2 * height, 2))
+        direction = random.choice(DIRECTIONS)
+        if 0 < x + direction[0] < 2 * width and 0 < y + direction[1] < 2 * height:
+            maze[x + direction[0] // 2][y + direction[1] // 2] = 0
 
-    return labyrinth
+    return maze
 
 
-def draw_labyrinth(labyrinth, wall_thickness):
+
+
+# Updated drawing function
+def draw_updated_labyrinth(labyrinth, wall_thickness):
     width = len(labyrinth)
     height = len(labyrinth[0])
     img = Image.new('RGB', (width, height), color='white')
@@ -51,19 +64,13 @@ def draw_labyrinth(labyrinth, wall_thickness):
         for j in range(height):
             if labyrinth[i][j] == 1:
                 draw.point((i, j), fill='black')
-
-    apple = "🍏"
-    font_path = "/usr/share/fonts/truetype/noto/NotoSans-Bold.ttf"
-    font_size = min(width, height) // 5
-    font = ImageFont.truetype(font_path, font_size)
-    text_bbox = draw.textbbox((0, 0), apple, font=font)
-    text_width = text_bbox[2] - text_bbox[0]
-    text_height = text_bbox[3] - text_bbox[1]
-    draw.text(((width - text_width) // 2, (height - text_height) // 2), apple, font=font, fill='red')
-
+    goal = "A"
+    font = ImageFont.load_default()
+    text_width, text_height = font.getsize(goal)
+    draw.text(((width - text_width) // 2, (height - text_height) // 2), goal, font=font, fill='red')
     return img
 
-
+# Updated main function
 def main():
     parser = argparse.ArgumentParser(description='Generate a labyrinth')
     parser.add_argument('width', type=int, help='Width of the labyrinth')
@@ -72,9 +79,8 @@ def main():
     width = args.width
     height = args.height
     wall_thickness = 3
-
-    labyrinth = generate_labyrinth(width, height, wall_thickness)
-    img = draw_labyrinth(labyrinth, wall_thickness)
+    labyrinth = generate_iterative_random_maze(width, height, wall_thickness)
+    img = draw_updated_labyrinth(labyrinth, wall_thickness)
     img.save("labyrinth.png")
     print("Labyrinth saved to labyrinth.png")
 
