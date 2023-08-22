fcd5cf7b807574da9499d36871c878207a76bcdc
Marcel Petrick <mail@marcelpetrick.it>
2023-08-22 15:48:35 +0200

    labyrinthMaker: solver added -  still has problem with even the simplest mazes

--- labyrinthMaker/labyrinthSolver.py
+++ labyrinthMaker/labyrinthSolver.py
@@ -1,75 +1,48 @@
 from PIL import Image, ImageOps, ImageDraw
-from queue import PriorityQueue
+import numpy as np
+from mazelib import Maze
+from mazelib.generate.Prims import Prims
+from mazelib.solve.BacktrackingDFS import BacktrackingDFS
 
-class MazeSolver:
-    def __init__(self, image_path):
-        self.image_path = image_path
-        self.maze = self.load_maze()
-        self.start = (1, 0)
-        self.goal = (len(self.maze) // 2, len(self.maze[0]) // 2)
-        self.solution = []
 
-    def load_maze(self):
-        img = ImageOps.grayscale(Image.open(self.image_path))
-        return [[0 if img.getpixel((x, y)) == 255 else 1 for y in range(img.height)] for x in range(img.width)]
+def image_to_grid(img_path):
+    """Convert a maze image to a 2D grid."""
+    img = ImageOps.grayscale(Image.open(img_path))
+    width, height = img.size
+    grid = [[0 if img.getpixel((x, y)) == 255 else 1 for y in range(height)] for x in range(width)]
+    return grid
 
-    @staticmethod
-    def is_goal(node, goal):
-        return node == goal
 
-    @staticmethod
-    def heuristic(node, goal):
-        return abs(node[0] - goal[0]) + abs(node[1] - goal[1])
+def solve_with_mazelib(grid):
+    """Solve the maze using mazelib."""
+    m = Maze()
+    m.grid = np.array(grid)
+    m.start = (1, 0)
+    m.end = (len(grid) // 2, len(grid[0]) // 2)
+    m.solver = BacktrackingDFS()
+    return m.solve()
 
-    def get_neighbors(self, node):
-        neighbors = []
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        for dx, dy in directions:
-            x, y = node[0] + dx, node[1] + dy
-            if 0 <= x < len(self.maze) and 0 <= y < len(self.maze[0]) and self.maze[x][y] == 0:
-                neighbors.append((x, y))
-        return neighbors
 
-    def solve(self):
-        open_list = PriorityQueue()
-        open_list.put((0, self.start))
-        came_from = {}
-        g_score = {(x, y): float('inf') for x in range(len(self.maze)) for y in range(len(self.maze[0]))}
-        g_score[self.start] = 0
-        f_score = {(x, y): float('inf') for x in range(len(self.maze)) for y in range(len(self.maze[0]))}
-        f_score[self.start] = self.heuristic(self.start, self.goal)
+def draw_solution_on_image(img_path, solution):
+    """Draw the solution path on the maze image."""
+    img = Image.open(img_path)
+    draw = ImageDraw.Draw(img)
+    if solution:
+        draw.line(solution, fill="blue", width=2)
+    img.save("labyrinth_solved_with_mazelib.png")
+    img.show()
 
-        while not open_list.empty():
-            _, current = open_list.get()
-            if self.is_goal(current, self.goal):
-                path = []
-                while current in came_from:
-                    path.append(current)
-                    current = came_from[current]
-                self.solution = path[::-1]
-                return self.solution
-            for neighbor in self.get_neighbors(current):
-                temp_g_score = g_score[current] + 1
-                if temp_g_score < g_score[neighbor]:
-                    came_from[neighbor] = current
-                    g_score[neighbor] = temp_g_score
-                    f_score[neighbor] = temp_g_score + self.heuristic(neighbor, self.goal)
-                    open_list.put((f_score[neighbor], neighbor))
-        return []
-
-    def draw_solution(self):
-        img = Image.open(self.image_path)
-        draw = ImageDraw.Draw(img)
-        if self.solution:
-            draw.line(self.solution, fill="blue", width=2)
-        return img
 
 def main():
-    solver = MazeSolver("labyrinth.png")
-    solver.solve()
-    solved_img = solver.draw_solution()
-    #solved_img.show()
-    solved_img.save("labyrinth_solved.png")
+    # Convert the maze image to a 2D grid
+    grid = image_to_grid("labyrinth.png")
+
+    # Solve the maze using mazelib
+    solution = solve_with_mazelib(grid)
+
+    # Draw the solution on the image
+    draw_solution_on_image("labyrinth.png", solution)
+
 
 if __name__ == '__main__':
     main()
