feff7a8050e2973fdb47f0529a6b1ccb56ce9276
Marcel Petrick <mail@marcelpetrick.it>
2023-08-25 16:21:05 +0200

    gitMailmapConsolidator.py: refining, but still more or less useless

--- gitMailmapConsolidator/gitMailmapConsolidator.py
+++ gitMailmapConsolidator/gitMailmapConsolidator.py
@@ -3,13 +3,20 @@ from collections import defaultdict
 
 
 def process_input(input_data):
+    """
+    Process the input data from `git shortlog -sne`.
+
+    :param list input_data: List of lines from `git shortlog -sne` output.
+    :return: A dictionary with email as the key and a list of (count, name) as values.
+    :rtype: dict
+    """
     # Store the count, name, and email
     data = []
     for line in input_data:
-        parts = line.strip().split("\t")
+        parts = line.strip().split()
         count = int(parts[0])
-        name, email = parts[1].rsplit(' ', 1)
-        email = email.strip('<>')
+        name = ' '.join(parts[1:-1])
+        email = parts[-1].strip('<>')
         data.append((count, name, email))
 
     # Create a dictionary with email as the key and a list of (count, name) as values
@@ -17,25 +24,38 @@ def process_input(input_data):
     for count, name, email in data:
         email_map[email].append((count, name))
 
-    # Determine the most common name for each email
-    result = {}
+    return email_map
+
+
+def generate_mailmap(email_map):
+    """
+    Generate the .mailmap entries based on the processed data.
+
+    :param dict email_map: Dictionary with email as key and list of (count, name) as values.
+    :return: Consolidated .mailmap entries.
+    :rtype: str
+    """
+    mailmap_entries = []
+
     for email, entries in email_map.items():
         entries.sort(reverse=True)  # Sort by count
         most_common_name = entries[0][1]
-        result[most_common_name] = email
 
-    return result
+        # Add the most common name-email pair as a "base" entry
+        mailmap_entries.append(f"{most_common_name} <{email}>")
 
-
-def generate_mailmap(data):
-    mailmap_entries = []
-    for name, email in data.items():
-        mailmap_entries.append(f"{name} <{email}>")
+        # Map all other names and emails to the most common one
+        for _, name in entries:
+            if name != most_common_name:
+                mailmap_entries.append(f"{most_common_name} <{email}> {name} <{email}>")
 
     return "\n".join(mailmap_entries)
 
 
 if __name__ == "__main__":
+    """
+    Main execution point. Takes input from stdin, processes it, and prints the .mailmap entries.
+    """
     input_data = [line for line in sys.stdin]
     processed_data = process_input(input_data)
     mailmap = generate_mailmap(processed_data)
