357fae3783165dd99168961eee39c53b92dab0f5
Marcel Petrick <mail@marcelpetrick.it>
2023-08-30 14:25:10 +0200

    translationMergerForTsFiles: working "finder"

--- translationMergerForTsFiles/translationMerger.py
+++ translationMergerForTsFiles/translationMerger.py
@@ -15,3 +15,42 @@
 # save resulting file
 #
 #-------------------------------------------
+
+
+import sys
+import xml.etree.ElementTree as ET
+
+def find_unfinished_entries(input_file):
+    """
+    Finds all 'unfinished' entries in a .ts file and prints them to stdout.
+
+    :param input_file: The path to the .ts file to read.
+    :type input_file: str
+    """
+    tree = ET.parse(input_file)
+    root = tree.getroot()
+
+    for message in root.iter('message'):
+        translation = message.find('translation')
+        if translation is not None and translation.attrib.get('type') == 'unfinished':
+            source_text = message.find('source').text
+            print(source_text)
+
+def main():
+    """
+    The main entry point of the script. It checks if the input and output file paths
+    were given as command line arguments and if so, calls the function to find
+    unfinished entries in the input .ts file.
+    """
+    if len(sys.argv) < 3:
+        print("Usage: python script.py input=inputfile.ts output=out.ts")
+        return
+
+    input_file = sys.argv[1].split('=')[1]
+    output_file = sys.argv[2].split('=')[1]  # output file is not used in this script
+
+    find_unfinished_entries(input_file)
+
+# $ python translationMerger.py input=recipebook_en_GB.ts output=230802_XBO_Automatikprogramme_en_GB-de-en_gb-QA-C.ts
+if __name__ == "__main__":
+    main()
