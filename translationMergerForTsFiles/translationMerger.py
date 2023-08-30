f1783a54da6f8a88da60174f08854ec0ddbb49c3
Marcel Petrick <mail@marcelpetrick.it>
2023-08-30 14:29:06 +0200

    translationMergerForTsFiles: reanming of the given parameters

--- translationMergerForTsFiles/translationMerger.py
+++ translationMergerForTsFiles/translationMerger.py
@@ -3,7 +3,7 @@
 # * import take the current db from TDM
 # * import the given ts-files (`source`)
 # * export the ts-files (`target`)
-# * create program which does the matchin from `source` to `target` and fixes the `target`-file
+# * create program which does the matching from `source` to `target` and fixes the `target`-file
 #
 #-------------------------------------------
 #
@@ -15,42 +15,94 @@
 # save resulting file
 #
 #-------------------------------------------
-
-
 import sys
 import xml.etree.ElementTree as ET
 
-def find_unfinished_entries(input_file):
+def find_unfinished_entries(target_tree):
     """
-    Finds all 'unfinished' entries in a .ts file and prints them to stdout.
+    Finds all 'unfinished' entries in a .ts file.
 
-    :param input_file: The path to the .ts file to read.
-    :type input_file: str
+    :param target_tree: The ElementTree object of the target .ts file.
+    :type target_tree: ET.ElementTree
+    :return: A list of 'source' texts of unfinished entries.
+    :rtype: list of str
     """
-    tree = ET.parse(input_file)
-    root = tree.getroot()
+    unfinished_entries = []
+    root = target_tree.getroot()
 
     for message in root.iter('message'):
         translation = message.find('translation')
         if translation is not None and translation.attrib.get('type') == 'unfinished':
             source_text = message.find('source').text
-            print(source_text)
+            unfinished_entries.append(source_text)
+
+    return unfinished_entries
+
+def update_translations(target_tree, source_tree, unfinished_entries):
+    """
+    Updates the translations in the target .ts file with the translations from
+    the source .ts file.
+
+    :param target_tree: The ElementTree object of the target .ts file.
+    :type target_tree: ET.ElementTree
+    :param source_tree: The ElementTree object of the source .ts file.
+    :type source_tree: ET.ElementTree
+    :param unfinished_entries: A list of 'source' texts of unfinished entries.
+    :type unfinished_entries: list of str
+    """
+    target_root = target_tree.getroot()
+    source_root = source_tree.getroot()
+
+    for message in source_root.iter('message'):
+        source_text = message.find('source').text
+        if source_text in unfinished_entries:
+            translation = message.find('translation')
+            if translation is not None and translation.attrib.get('type') != 'unfinished':
+                translation_text = translation.text
+                update_target_translation(target_root, source_text, translation_text)
+
+def update_target_translation(target_root, source_text, translation_text):
+    """
+    Updates the translation of a specific 'source' text in the target .ts file
+    and removes the 'unfinished' attribute.
+
+    :param target_root: The root element of the target .ts file.
+    :type target_root: ET.Element
+    :param source_text: The 'source' text of the entry to update.
+    :type source_text: str
+    :param translation_text: The new 'translation' text.
+    :type translation_text: str
+    """
+    for message in target_root.iter('message'):
+        if message.find('source').text == source_text:
+            translation = message.find('translation')
+            translation.text = translation_text
+            if 'type' in translation.attrib:
+                del translation.attrib['type']
 
 def main():
     """
-    The main entry point of the script. It checks if the input and output file paths
-    were given as command line arguments and if so, calls the function to find
-    unfinished entries in the input .ts file.
+    The main entry point of the script. It checks if the target and source file paths
+    were given as command line arguments and if so, calls the functions to find
+    unfinished entries in the target .ts file, update them with the translations from
+    the source .ts file, and write the updated target .ts file back to disk.
     """
     if len(sys.argv) < 3:
-        print("Usage: python script.py input=inputfile.ts output=out.ts")
+        print("Usage: python script.py target=targetfile.ts source=source.ts")
         return
 
-    input_file = sys.argv[1].split('=')[1]
-    output_file = sys.argv[2].split('=')[1]  # output file is not used in this script
+    target_file = sys.argv[1].split('=')[1]
+    source_file = sys.argv[2].split('=')[1]
 
-    find_unfinished_entries(input_file)
+    target_tree = ET.parse(target_file)
+    source_tree = ET.parse(source_file)
 
-# $ python translationMerger.py input=recipebook_en_GB.ts output=230802_XBO_Automatikprogramme_en_GB-de-en_gb-QA-C.ts
+    unfinished_entries = find_unfinished_entries(target_tree)
+    update_translations(target_tree, source_tree, unfinished_entries)
+
+    target_tree.write(target_file, encoding='utf-8')
+
+
+# $ python translationMerger.py target=recipebook_en_GB.ts source=230802_XBO_Automatikprogramme_en_GB-de-en_gb-QA-C.ts
 if __name__ == "__main__":
     main()
