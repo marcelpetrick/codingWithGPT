91ffbaf85dfb6788bd3e85cbacb88af78f4f34bf
Marcel Petrick <mail@marcelpetrick.it>
2023-08-30 15:56:13 +0200

    simpleImageViewer: added the rquirements

---
+++ simpleImageViewer/simpleImageViewer.py
@@ -0,0 +1,5 @@
+# I need a python  script, which can read a given input folder, where several images files are located.
+# Load those files and display them full screen. When the user swipes on the touch-display to the left, load the next image, then the next and then start a new (just like for nay ring-buffer). For swiping right the previous image shall be shwon.
+# Libraries to use are free, but maybe stick at maximum to PyQt or something.
+# The program should not react to any other input. Just the swipes left and right (maybe with mouse event it would be drag left and right?).
+# Also the program should not show any window borders or something. Just the image full screen.
