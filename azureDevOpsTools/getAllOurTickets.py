7edd69a9689f39ce0ec6772a321e0c286efc5099
Marcel Petrick <mail@marcelpetrick.it>
2023-09-05 16:05:24 +0200

    Azure: getAllOurTickets.py: working code, but still needs some beautification

--- azureDevOpsTools/getAllOurTickets.py
+++ azureDevOpsTools/getAllOurTickets.py
@@ -34,17 +34,53 @@ class AzureDevOpsClient:
         wiql_object = Wiql(query=wiql)
         return wit_client.query_by_wiql(wiql_object).work_items
 
+    @staticmethod
+    def _parse_date(date_str):
+        try:
+            # Try parsing without fractional seconds first
+            return datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ')
+        except ValueError:
+            # If the above fails, try parsing with fractional seconds
+            return datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+
     def parse_metadata_from_ticket(self, work_item):
         wit_client = self.connection.clients.get_work_item_tracking_client()
         work_item_result = wit_client.get_work_item(work_item.id)
         fields = work_item_result.fields
+
+        # Convert dates to the desired format
+        created_date_str = fields.get('System.CreatedDate', None)
+        if created_date_str:
+            created_date_dt = self._parse_date(created_date_str)
+            created_date = created_date_dt.strftime('%d.%m.%Y')
+        else:
+            created_date = None
+
+        resolved_date_str = fields.get('Microsoft.VSTS.Common.ResolvedDate', None)
+        if resolved_date_str:
+            resolved_date_dt = self._parse_date(resolved_date_str)
+            resolved_date = resolved_date_dt.strftime('%d.%m.%Y')
+        else:
+            resolved_date = None
+
+        closed_date_str = fields.get('Microsoft.VSTS.Common.ClosedDate', None)
+        if closed_date_str:
+            closed_date_dt = self._parse_date(closed_date_str)
+            closed_date = closed_date_dt.strftime('%d.%m.%Y')
+        else:
+            closed_date = None
+
+        # Modify title and create URL
+        title = f"{work_item_result.id}: {fields.get('System.Title', '')}"
+        url = f"https://dev.azure.com/bora-devops/P118_HMI/_workitems/edit/{work_item_result.id}/"
+
         return {
-            'Title': fields.get('System.Title', None),
-            'URL': work_item_result.url,
+            'Title': title,
+            'URL': url,
             'ID': work_item_result.id,
-            'Created Date': fields['System.CreatedDate'],
-            'Resolved Date': fields.get('Microsoft.VSTS.Common.ResolvedDate', None),
-            'Closed Date': fields.get('Microsoft.VSTS.Common.ClosedDate', None),
+            'Created Date': created_date,
+            'Resolved Date': resolved_date,
+            'Closed Date': closed_date,
             'Work Item Type': fields.get('System.WorkItemType', None)
         }
 
