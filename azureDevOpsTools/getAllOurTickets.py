fd713d5a41ce87ced80346c78c07a2cb3d8bb297
Marcel Petrick <mail@marcelpetrick.it>
2023-09-05 17:19:39 +0200

    Azure: getAllOurTickets.py: documented code

--- azureDevOpsTools/getAllOurTickets.py
+++ azureDevOpsTools/getAllOurTickets.py
@@ -1,11 +1,28 @@
 from azure.devops.connection import Connection
-from msrest.authentication import BasicAuthentication
 from azure.devops.v7_0.work_item_tracking.models import Wiql
+from msrest.authentication import BasicAuthentication
 from datetime import datetime
 
 
 class AzureDevOpsClient:
+    """
+    A client for interacting with Azure DevOps using a Personal Access Token (PAT).
+
+    Attributes:
+        pat (str): Personal Access Token.
+        organization_url (str): Azure DevOps organization URL.
+        credentials: Azure DevOps credentials.
+        connection: Azure DevOps connection object.
+    """
+
     def __init__(self, pat_file, organization_url):
+        """
+        Initialize AzureDevOpsClient.
+
+        Args:
+            pat_file (str): File path to the PAT file.
+            organization_url (str): Azure DevOps organization URL.
+        """
         self.pat = self._get_pat_from_file(pat_file)
         self.organization_url = organization_url
         self.credentials = BasicAuthentication('', self.pat)
@@ -13,29 +30,32 @@ class AzureDevOpsClient:
 
     @staticmethod
     def _get_pat_from_file(file_path):
+        """Retrieve PAT from the given file."""
         with open(file_path, 'r') as file:
             return file.read().strip()
 
     def get_project(self, index=1):
+        """Get the project at the specified index."""
         core_client = self.connection.clients.get_core_client()
         get_projects_response = core_client.get_projects()
         return get_projects_response[index]
 
     def query_work_items(self, project_name, tags):
+        """Query work items for a given project name and tags."""
         wit_client = self.connection.clients.get_work_item_tracking_client()
         wiql = """
         SELECT [System.Id], [System.Title], [System.State]
         FROM workitems
-        WHERE [System.TeamProject] = @project
-        """
+        WHERE [System.TeamProject] = '{}'
+        """.format(project_name)
         for tag in tags:
-            wiql += f"AND [System.Tags] CONTAINS '{tag}'\n"
-        wiql = wiql.replace("@project", f"'{project_name}'")
+            wiql += "AND [System.Tags] CONTAINS '{}'\n".format(tag)
         wiql_object = Wiql(query=wiql)
         return wit_client.query_by_wiql(wiql_object).work_items
 
     @staticmethod
     def _parse_date(date_str):
+        """Parse a date string into a datetime object, handling multiple formats."""
         try:
             # Try parsing without fractional seconds first
             return datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ')
@@ -44,31 +64,20 @@ class AzureDevOpsClient:
             return datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%fZ')
 
     def parse_metadata_from_ticket(self, work_item):
+        """Parse metadata from a given work item."""
         wit_client = self.connection.clients.get_work_item_tracking_client()
         work_item_result = wit_client.get_work_item(work_item.id)
         fields = work_item_result.fields
 
+        # Helper function for date conversion
+        def _get_date(field_name):
+            date_str = fields.get(field_name, None)
+            return self._parse_date(date_str).strftime('%d.%m.%Y') if date_str else None
+
         # Convert dates to the desired format
-        created_date_str = fields.get('System.CreatedDate', None)
-        if created_date_str:
-            created_date_dt = self._parse_date(created_date_str)
-            created_date = created_date_dt.strftime('%d.%m.%Y')
-        else:
-            created_date = None
-
-        resolved_date_str = fields.get('Microsoft.VSTS.Common.ResolvedDate', None)
-        if resolved_date_str:
-            resolved_date_dt = self._parse_date(resolved_date_str)
-            resolved_date = resolved_date_dt.strftime('%d.%m.%Y')
-        else:
-            resolved_date = None
-
-        closed_date_str = fields.get('Microsoft.VSTS.Common.ClosedDate', None)
-        if closed_date_str:
-            closed_date_dt = self._parse_date(closed_date_str)
-            closed_date = closed_date_dt.strftime('%d.%m.%Y')
-        else:
-            closed_date = None
+        created_date = _get_date('System.CreatedDate')
+        resolved_date = _get_date('Microsoft.VSTS.Common.ResolvedDate')
+        closed_date = _get_date('Microsoft.VSTS.Common.ClosedDate')
 
         # Modify title and create URL
         title = f"{work_item_result.id}: {fields.get('System.Title', '')}"
@@ -86,8 +95,11 @@ class AzureDevOpsClient:
 
 
 class FileWriter:
+    """A simple writer to save data to a file."""
+
     @staticmethod
     def write_list_of_dict_to_file(data):
+        """Write a list of dictionaries to a file."""
         timestamp = datetime.now().strftime("%Y%m%d_%H%M")
         filename = f"tickets_DMO_toImplement_{timestamp}.txt"
         with open(filename, 'w') as file:
@@ -105,6 +117,6 @@ if __name__ == '__main__':
     for item in parsed_work_items:
         print(item)
 
-    print(f"amount of found tickets: {len(parsed_work_items)}")
+    print(f"Amount of found tickets: {len(parsed_work_items)}")
 
     FileWriter.write_list_of_dict_to_file(parsed_work_items)
