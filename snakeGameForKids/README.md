d0383379d5e753708eff5a5ae5250a8fe8ac6b41
Marcel Petrick <mail@marcelpetrick.it>
2023-11-11 13:06:31 +0100

    snakeGameForKids: randomly spawned fruits are now a feature; also added a readme.md

---
+++ snakeGameForKids/README.md
@@ -0,0 +1,32 @@
+# Snake Game for Kids
+
+## What is This?
+The Snake Game for Kids is a delightful twist on the classic snake game, designed to be both fun and visually engaging for children. It features a **rainbow-colored snake** that navigates through a blocky, colorful world, growing as it eats randomly spawning fruits. These fruits appear in vibrant colors of orange, blue, and cyan, adding an element of surprise and excitement to the game. The simple yet charming blocky appearance of the game elements makes it appealing and accessible for young gamers.
+
+## How to Configure the Setup
+To get started with the Snake Game for Kids, you need to have Python 3.11 installed on your system. The game relies on the Pygame library, which can be installed via the `requirements.txt` file provided with the game. Follow these steps to set up the game environment:
+
+1. Ensure you have Python 3.11 installed.
+2. Clone or download the game repository to your local machine.
+3. Navigate to the game directory in your terminal or command prompt.
+4. Run the command `pip install -r requirements.txt` to install the necessary Pygame library.
+
+## How to Run?
+To start the game, follow these simple steps:
+
+1. Open your terminal or command prompt.
+2. Navigate to the directory where the game files are located.
+3. Run the command `python snakeGameForKids.py` to start the game.
+
+## Gameplay
+Control the snake using the arrow keys:
+- **Up Arrow**: Move the snake upwards.
+- **Down Arrow**: Move the snake downwards.
+- **Left Arrow**: Move the snake to the left.
+- **Right Arrow**: Move the snake to the right.
+- **Spacebar**: Temporarily double the snake's speed.
+- **Enter Key**: Pause and resume the game.
+
+The game ends when the snake collides with itself. Enjoy navigating the snake through a world of colorful fruits and rainbow segments!
+
+Author: Marcel Petrick (mail@marcelpetrick.it)
--- snakeGameForKids/snakeGameForKids.py
+++ snakeGameForKids/snakeGameForKids.py
@@ -38,6 +38,25 @@ def our_snake(snake_block, snake_list):
         color = rainbow_colors[i % len(rainbow_colors)]  # Cycle through the rainbow colors
         pygame.draw.rect(dis, color, [x[0], x[1], snake_block, snake_block])
 
+# Fruit colors
+fruit_colors = [
+    (255, 165, 0),  # Orange
+    (0, 0, 255),    # Blue
+    (0, 255, 255),  # Cyan
+]
+
+# Function to spawn a new fruit
+def spawn_fruit():
+    if len(fruits) < 3:  # Limit to 3 fruits
+        x = round(random.randrange(0, dis_width - snake_block) / snake_block) * snake_block
+        y = round(random.randrange(0, dis_height - snake_block) / snake_block) * snake_block
+        color = random.choice(fruit_colors)
+        fruits.append({'pos': (x, y), 'color': color})
+        #print(fruits)
+
+# Initial fruits list
+fruits = []
+
 # Main game loop
 def gameLoop():
     global  snake_speed # Add this line to declare snake_speed as non-local
@@ -56,9 +75,9 @@ def gameLoop():
     snake_List = []
     Length_of_snake = 1
 
-    # Location of the food
-    foodx = round(random.randrange(0, dis_width - snake_block) / snake_block) * snake_block
-    foody = round(random.randrange(0, dis_height - snake_block) / snake_block) * snake_block
+    # Initial spawn of fruits
+    while len(fruits) < 3:
+        spawn_fruit()
 
     # Game loop
     while not game_over:
@@ -118,7 +137,7 @@ def gameLoop():
         x1 += x1_change
         y1 += y1_change
         dis.fill(blue)
-        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
+
         snake_Head = []
         snake_Head.append(x1)
         snake_Head.append(y1)
@@ -131,14 +150,24 @@ def gameLoop():
                 game_close = True
 
         our_snake(snake_block, snake_List)
+
+        # Spawn new fruit randomly
+        if random.randint(1, 20) == 1:  # Adjust the probability as needed
+            spawn_fruit()
+
+        # Draw the fruits
+        for fruit in fruits:
+            pygame.draw.rect(dis, fruit['color'], [fruit['pos'][0], fruit['pos'][1], snake_block, snake_block])
+
         pygame.display.update()
 
-        # Snake eating food
-        if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, dis_width - snake_block) / snake_block) * snake_block
-            foody = round(random.randrange(0, dis_height - snake_block) / snake_block) * snake_block
-            Length_of_snake += 1
-            snake_speed = 15  # Reset speed after eating
+        # Check if the snake has eaten a fruit
+        snake_head = (x1, y1)
+        for fruit in fruits[:]:  # Iterate over a copy of the list
+            if snake_head == fruit['pos']:
+                fruits.remove(fruit)  # Remove the eaten fruit
+                Length_of_snake += 1  # Increase the length of the snake
+                snake_speed = 10 # Reset the snake speed
 
         pygame.time.Clock().tick(snake_speed)
 
