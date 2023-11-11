85a3528c0eea38276099afdbe451f00dd00902d1
Marcel Petrick <mail@marcelpetrick.it>
2023-11-11 12:48:10 +0100

    snakeGameForKids: modifications for visual appearance, speed and general refactoring of constant-usage

--- snakeGameForKids/snakeGameForKids.py
+++ snakeGameForKids/snakeGameForKids.py
@@ -18,11 +18,11 @@ dis_height = 600
 
 # Create the display
 dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game for Kids')
+pygame.display.set_caption('Snake Game for my children')
 
 # Set the snake block size and speed
-snake_block = 10
-snake_speed = 15
+snake_block = 20
+snake_speed = 10
 
 # Define the snake
 def our_snake(snake_block, snake_list):
@@ -48,15 +48,15 @@ def gameLoop():
     Length_of_snake = 1
 
     # Location of the food
-    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+    foodx = round(random.randrange(0, dis_width - snake_block) / snake_block) * snake_block
+    foody = round(random.randrange(0, dis_height - snake_block) / snake_block) * snake_block
 
     # Game loop
     while not game_over:
         while game_close == True:
             dis.fill(blue)
             font_style = pygame.font.SysFont(None, 50)
-            mesg = font_style.render("You Lost! Press C-Play Again or Q-Quit", True, red)
+            mesg = font_style.render("You Lost! Press (P)lay Again or (Q)uit", True, red)
             dis.blit(mesg, [dis_width / 6, dis_height / 3])
 
             pygame.display.update()
@@ -66,7 +66,7 @@ def gameLoop():
                     if event.key == pygame.K_q:
                         game_over = True
                         game_close = False
-                    if event.key == pygame.K_c:
+                    if event.key == pygame.K_p:
                         gameLoop()
 
         for event in pygame.event.get():
@@ -86,7 +86,7 @@ def gameLoop():
                     y1_change = snake_block
                     x1_change = 0
                 elif event.key == pygame.K_SPACE:  # Press space for double speed
-                    snake_speed = 30
+                    snake_speed = snake_speed * 2
                 elif event.key == pygame.K_RETURN:  # Press enter to pause
                     pause = True
                     while pause:
@@ -126,8 +126,8 @@ def gameLoop():
 
         # Snake eating food
         if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            foodx = round(random.randrange(0, dis_width - snake_block) / snake_block) * snake_block
+            foody = round(random.randrange(0, dis_height - snake_block) / snake_block) * snake_block
             Length_of_snake += 1
             snake_speed = 15  # Reset speed after eating
 
