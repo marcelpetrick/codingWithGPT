2a278c8acfe3adee1e2a5f8f0081edd4f03f4dce
Marcel Petrick <mail@marcelpetrick.it>
2023-12-03 23:35:13 +0100

    geoLocViz: can plot the map

--- geoLocViz/geoLocViz.py
+++ geoLocViz/geoLocViz.py
@@ -1,26 +1,62 @@
 from geopy.exc import GeocoderTimedOut
 from geopy.geocoders import Photon
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+import numpy as np
 
 def get_location_coordinates(location_name):
     geolocator = Photon(user_agent="measurements")
 
     try:
-        # Get location data
         location = geolocator.geocode(location_name)
-
-        # Check if location is found
         if location:
             return (location.latitude, location.longitude)
         else:
-            return ("Location not found", "Location not found")
+            return (None, None)
     except GeocoderTimedOut:
-        return ("Geocoder service timed out", "Geocoder service timed out")
+        return (None, None)
 
-def evaluateLocations(input):
-    for location in input:
-        print(f"location: {location}")
+def plot_locations_on_map(locations):
+    # Initialize lists to store latitude and longitude
+    lats, lons = [], []
+
+    # Geocode each location and add to lists
+    for location in locations:
         lat, lon = get_location_coordinates(location)
-        print(f"lat: {lat}, lon: {lon})")
+        if lat is not None and lon is not None:
+            lats.append(lat)
+            lons.append(lon)
+            print(f"location: {location}, lat: {lat}, lon: {lon}")
 
-locations = ["Eiffel Tower", "Eibsee", "Zorneding"]
-evaluateLocations(locations)
+    # Calculate margins dynamically
+    lat_margin = max(lats) - min(lats)
+    lon_margin = max(lons) - min(lons)
+    margin_ratio = 1.0  # 200% margin
+
+    # Create a new map
+    fig = plt.figure(figsize=(10, 5))
+    m = Basemap(projection='merc',
+                llcrnrlat=min(lats) - lat_margin * margin_ratio,
+                urcrnrlat=max(lats) + lat_margin * margin_ratio,
+                llcrnrlon=min(lons) - lon_margin * margin_ratio,
+                urcrnrlon=max(lons) + lon_margin * margin_ratio,
+                lat_ts=20, resolution='i')
+
+    m.drawcoastlines()
+    m.drawcountries()
+    m.fillcontinents(color='lightgray', lake_color='lightblue')
+    m.drawmapboundary(fill_color='lightblue')
+
+    # Convert latitude and longitude to x and y coordinates
+    x, y = m(lons, lats)
+
+    # Plot each location on the map
+    #m.scatter(x, y, marker='x', color='r', zorder=5)
+    m.scatter(x, y, marker='^', color='r', zorder=5)
+
+    plt.savefig("map.png", format='png', dpi=600)
+    #plt.show()
+
+# Locations to plot
+locations = ["Regensburg", "München", "Eibsee", "Zorneding"]
+plot_locations_on_map(locations)
