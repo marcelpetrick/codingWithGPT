9f34721423de4f7dcf776dc14e90f162e62ca085
Marcel Petrick <mail@marcelpetrick.it>
2023-12-05 23:47:08 +0100

    geoLocViz: refactored into OOP with docstrings

--- geoLocViz/geoLocViz.py
+++ geoLocViz/geoLocViz.py
@@ -1,93 +1,128 @@
 import json
-import sys
-import re
 from pathlib import Path
 from geopy.exc import GeocoderTimedOut
 from geopy.geocoders import Photon
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
+import re
+import sys
 
-# Global cache file path
-CACHE_FILE_PATH = "geocache.json"
+class GeoCache:
+    """
+    A class for handling geolocation caching and plotting.
 
-def load_cache():
+    Attributes:
+        cache_file (str): Path to the cache file.
+        cache (dict): Cache storage for geolocations.
     """
-    Load the cache file if it exists, otherwise return an empty dictionary.
-    """
-    if Path(CACHE_FILE_PATH).is_file():
-        with open(CACHE_FILE_PATH, 'r') as file:
-            return json.load(file)
-    return {}
 
-def save_cache(cache):
-    """
-    Save the updated cache to the file.
-    """
-    with open(CACHE_FILE_PATH, 'w') as file:
-        json.dump(cache, file, indent=4)
+    def __init__(self, cache_file="geocache.json"):
+        """
+        Initializes the GeoCache object with a specified cache file.
 
-def get_location_coordinates(location_name, cache):
-    """
-    Get coordinates for a location, using the cache if available,
-    otherwise querying the geolocation service.
-    """
-    if location_name in cache:
-        print(f"Cache hit for {location_name}")
-        return cache[location_name]
+        Args:
+            cache_file (str): Path to the cache file. Defaults to 'geocache.json'.
+        """
+        self.cache_file = cache_file
+        self.cache = self.load_cache()
 
-    print(f"Cache miss for {location_name}")
-    geolocator = Photon(user_agent="measurements")
-    try:
-        location = geolocator.geocode(location_name)
-        if location:
-            cache[location_name] = (location.latitude, location.longitude)
-            save_cache(cache)  # Save cache immediately after adding new location
-            return cache[location_name]
-        else:
+    def load_cache(self):
+        """
+        Load the cache file if it exists, otherwise return an empty dictionary.
+
+        Returns:
+            dict: The loaded cache.
+        """
+        if Path(self.cache_file).is_file():
+            with open(self.cache_file, 'r') as file:
+                return json.load(file)
+        return {}
+
+    def save_cache(self):
+        """
+        Save the updated cache to the file.
+        """
+        with open(self.cache_file, 'w') as file:
+            json.dump(self.cache, file, indent=4)
+
+    def get_location_coordinates(self, location_name):
+        """
+        Get coordinates for a location, using the cache if available,
+        otherwise querying the geolocation service.
+
+        Args:
+            location_name (str): The name of the location to find coordinates for.
+
+        Returns:
+            tuple: A tuple of latitude and longitude.
+        """
+        if location_name in self.cache:
+            print(f"Cache hit for {location_name}")
+            return self.cache[location_name]
+
+        print(f"Cache miss for {location_name}")
+        geolocator = Photon(user_agent="measurements")
+        try:
+            location = geolocator.geocode(location_name)
+            if location:
+                self.cache[location_name] = (location.latitude, location.longitude)
+                self.save_cache()  # Save cache immediately after adding new location
+                return self.cache[location_name]
+            else:
+                return (None, None)
+        except GeocoderTimedOut:
             return (None, None)
-    except GeocoderTimedOut:
-        return (None, None)
 
-def plot_locations_on_map(locations, cache):
-    """
-    Plot the given locations on a map and save it as a PNG file.
-    """
-    lats, lons = [], []
-    for location in locations:
-        lat, lon = get_location_coordinates(location, cache)
-        if lat is not None and lon is not None:
-            lats.append(lat)
-            lons.append(lon)
-            print(f"location: {location}, lat: {lat}, lon: {lon}")
+    def plot_locations_on_map(self, locations):
+        """
+        Plot the given locations on a map and save it as a PNG file.
 
-    if not lats or not lons:
-        print("No valid locations to plot.")
-        return
+        Args:
+            locations (list): A list of location names to be plotted.
+        """
+        lats, lons = [], []
+        for location in locations:
+            lat, lon = self.get_location_coordinates(location)
+            if lat is not None and lon is not None:
+                lats.append(lat)
+                lons.append(lon)
+                print(f"location: {location}, lat: {lat}, lon: {lon}")
 
-    lat_margin = max(lats) - min(lats)
-    lon_margin = max(lons) - min(lons)
-    margin_ratio = 1.0
+        if not lats or not lons:
+            print("No valid locations to plot.")
+            return
 
-    fig = plt.figure(figsize=(10, 5))
-    m = Basemap(projection='merc', llcrnrlat=min(lats) - lat_margin * margin_ratio,
-                urcrnrlat=max(lats) + lat_margin * margin_ratio,
-                llcrnrlon=min(lons) - lon_margin * margin_ratio,
-                urcrnrlon=max(lons) + lon_margin * margin_ratio,
-                lat_ts=20, resolution='i')
+        lat_margin = max(lats) - min(lats)
+        lon_margin = max(lons) - min(lons)
+        margin_ratio = 1.0
 
-    m.drawcoastlines()
-    m.drawcountries()
-    m.fillcontinents(color='lightgray', lake_color='lightblue')
-    m.drawmapboundary(fill_color='lightblue')
+        fig = plt.figure(figsize=(10, 5))
+        m = Basemap(projection='merc', llcrnrlat=min(lats) - lat_margin * margin_ratio,
+                    urcrnrlat=max(lats) + lat_margin * margin_ratio,
+                    llcrnrlon=min(lons) - lon_margin * margin_ratio,
+                    urcrnrlon=max(lons) + lon_margin * margin_ratio,
+                    lat_ts=20, resolution='i')
 
-    x, y = m(lons, lats)
-    m.scatter(x, y, marker='^', color='r', zorder=5)
+        m.drawcoastlines()
+        m.drawcountries()
+        m.fillcontinents(color='lightgray', lake_color='lightblue')
+        m.drawmapboundary(fill_color='lightblue')
 
-    plt.savefig("map.png", format='png', dpi=600)
+        x, y = m(lons, lats)
+        m.scatter(x, y, marker='^', color='r', zorder=5)
+
+        plt.savefig("map.png", format='png', dpi=600)
+
 
 def parse_location_file(file_path):
     """
     Parse a file containing dates and locations, returning a list of locations.
+
+    Args:
+        file_path (str): Path to the file containing dates and locations.
+
+    Returns:
+        list: A list of parsed locations.
     """
     locations = []
     with open(file_path, 'r') as file:
@@ -103,24 +138,21 @@ def parse_location_file(file_path):
 
 def main(file_path=None):
     """
-    Main function to load cache, parse locations from a file (if given),
+    Main function to create a GeoCache object, parse locations from a file,
     plot locations on the map, and save the updated cache.
+
+    Args:
+        file_path (str): Optional path to a file containing dates and locations.
     """
-    # Load cache
-    cache = load_cache()
+    geo_cache = GeoCache()
 
-    # Define locations
     locations = parse_location_file(file_path) if file_path else []
-
-    # Plot locations
-    plot_locations_on_map(locations, cache)
-
-    # Save cache
-    save_cache(cache)
+    geo_cache.plot_locations_on_map(locations)
+    geo_cache.save_cache()
 
 if __name__ == "__main__":
-    file_path = sys.argv[1] if len(sys.argv) > 1 else None
-    main(file_path)
+    file_path_arg = sys.argv[1] if len(sys.argv) > 1 else None
+    main(file_path_arg)
 
 # call with:
 # python geoLocViz.py touristlocations_example.md
