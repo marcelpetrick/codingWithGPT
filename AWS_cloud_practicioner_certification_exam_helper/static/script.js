e60a83dcc205e90fd3b761285e5ae226bac733f0
Marcel Petrick <mail@marcelpetrick.it>
2023-12-07 16:42:36 +0100

    AWS exam helper: shows at least the question and the options; but the slection is missing - wip

--- AWS_cloud_practicioner_certification_exam_helper/static/script.js
+++ AWS_cloud_practicioner_certification_exam_helper/static/script.js
@@ -12,16 +12,40 @@ function loadQuestion() {
 }
 
 function displayQuestion(question) {
-    // Implement the logic to display the question and options in the HTML
-    // For example:
+    // Display the question text
     document.getElementById('question').textContent = question.text;
-    // And similarly for options...
+
+    // Clear previous options
+    const optionsDiv = document.getElementById('options');
+    optionsDiv.innerHTML = '';
+
+    // Create and display options
+    question.options.forEach((option, index) => {
+        const button = document.createElement('button');
+        button.textContent = option;
+        button.classList.add('option');
+        button.addEventListener('click', () => submitAnswer(option, question));
+        optionsDiv.appendChild(button);
+    });
 }
 
-
-function submitAnswer(answer) {
-    // Implement logic to submit answer and handle response
+function submitAnswer(selectedOption, question) {
+    // AJAX call to submit the answer and handle the response
+    // Example:
+    fetch('/submit_answer', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ answer: selectedOption, questionText: question.text })
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Handle response (display feedback and explanation)
+    })
+    .catch(error => console.error('Error:', error));
 }
+
 
 // Add event listeners for keypress or button click events
 document.addEventListener('keydown', (event) => {
